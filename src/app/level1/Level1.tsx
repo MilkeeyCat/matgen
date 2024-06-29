16403065d0c00540c92aef635ceb7844d50ba0fa
Amir256456 <amir1008047@gmail.com>
2024-06-29 08:48:40 +0500

    models updated

--- src/app/level1/Level1.tsx
+++ src/app/level1/Level1.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { redirect } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { GameField } from '../gameField/GameField'
 import styles from './Level1.module.scss'
@@ -21,6 +22,10 @@ export function Level1() {
 	}, [level])
 
 	useEffect(() => {
+		if (typeof window !== 'undefined' && level === 4) {
+			redirect('/')
+		}
+
 		const calculateDistance = () => {
 			return window.innerWidth - 128 - botPosition - 280
 		}
@@ -28,7 +33,7 @@ export function Level1() {
 		const distance = calculateDistance()
 		setDistanceBetweenTanks(distance)
 		setSpeed(Math.floor(Number(distance / time)))
-	}, [time, level])
+	}, [time, level, botPosition])
 
 	const handleAttack = () => {
 		setIsAttack(true)
@@ -40,37 +45,52 @@ export function Level1() {
 	}
 
 	return (
-		<div className='w-screen h-screen '>
+		<div className="w-screen h-screen bg-contain bg-[url('/assets/background.png')]">
 			<div className={styles.problemSolve}>
-				{level === 1 && (
-					<p className={styles.problemText}>
-						DISTANCE: {distanceBetweenTanks} <br />
-						TIME: {time} <br />
-						VELOCITY: ??? (in integer, rounded to bottom)
-					</p>
-				)}
-				{level === 2 && (
-					<p className={styles.problemText}>
-						DISTANCE: {distanceBetweenTanks} <br />
-						VELOCITY: {speed} <br />
-						TIME: ??? (in integer, rounded to bottom)
-					</p>
-				)}
-				{level === 3 && (
-					<p className={styles.problemText}>
-						TIME: {time} <br />
-						VELOCITY: {speed} <br />
-						DISTANCE: ??? (in integer, rounded to bottom)
-					</p>
-				)}
-
-				<input
-					onChange={e => setAnswer(Number(e.target.value))}
-					placeholder='Speed'
-					type='number'
-					value={answer}
-				/>
-				<button onClick={() => handleAttack()}>Attack</button>
+				<div className={styles.controller}>
+					<button className={styles.controllerBtn}>{'					< back '}</button>
+					<div className={styles.level}>1.{level}</div>
+					<button
+						className={styles.controllerBtn}
+						onClick={() => handleAttack()}
+					>
+						{'fire >'}
+					</button>
+				</div>
+				<div className={styles.problemText}>
+					{level === 1 && (
+						<p>
+							If the distance between two tanks is {distanceBetweenTanks}{' '}
+							meters, what speed should the bullet have to hit the opponent in{' '}
+							{time} seconds?
+						</p>
+					)}
+					{level === 2 && (
+						<p>
+							If the distance between two tanks is {distanceBetweenTanks} meters
+							and the speed of th bullet is {speed} meters per second, what time
+							it will take the bullet to hit the opponent?
+						</p>
+					)}
+					{level === 3 && (
+						<p>
+							If the bullet hit the opponent tank in {time} seconds, flying at
+							the speed {speed} meters per second, what was the distance between
+							tanks?
+						</p>
+					)}
+					<div className={styles.field}>
+						<label>
+							Enter {level === 1 && 'v'} {level === 2 && 't'}{' '}
+							{level === 3 && 's'}
+						</label>
+						<input
+							onChange={e => setAnswer(Number(e.target.value))}
+							type='number'
+							value={answer}
+						/>
+					</div>
+				</div>
 			</div>
 
 			<GameField
--- src/app/level1/utils.ts
+++ src/app/level1/utils.ts
@@ -14,21 +14,21 @@ export const validate = ({
 	level,
 }: IValidate) => {
 	if (level === 1) {
-		if (Math.floor(speed) == answer) {
+		if (Math.floor(speed) === answer) {
 			return true
 		} else {
 			return false
 		}
 	}
 	if (level === 2) {
-		if (Math.floor(time) == answer) {
+		if (Math.floor(time) === answer) {
 			return true
 		} else {
 			return false
 		}
 	}
 	if (level === 3) {
-		if (Math.floor(distanceBetweenTanks) == answer) {
+		if (Math.abs(Math.floor(distanceBetweenTanks) - answer) < 10) {
 			return true
 		} else {
 			return false
